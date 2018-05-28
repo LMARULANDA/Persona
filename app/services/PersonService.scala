3403be19477711cbf351f9bfc9fc6657e84af6b8
unknown <laura.marulanda@ceiba.com.co>
2018-05-28 13:45:07 -0500

    Persona

---
+++ app/services/PersonService.scala
@@ -0,0 +1,21 @@
+package services
+
+import javax.inject.Inject
+import models.{Person, PersonRepository}
+import play.api.libs.json.Json
+import play.api.mvc.Result
+
+import scala.concurrent.Future
+
+class PersonService @Inject (personRepository: PersonRepository) {
+
+   def insertPerson(person: Person):Future[Result] = {
+    personRepository.create(person.name,person.age)
+      .map(e =>Ok(Json.toJson(e)))
+      .recoverWith {
+        case e : Exception => {
+          Future.successful(InternalServerError("No pudo guardarse el registro"))
+        }
+      }
+  }
+}
